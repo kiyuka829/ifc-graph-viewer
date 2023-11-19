1bd7ed14545c040d06bf7a5bfcc126c9efc9fdad
kiyuka <kiyuka.qwerty@gmail.com>
2023-11-19 11:06:44 +0900

    ファイル読み込み処理追加

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <script setup lang="ts">
-import Canvas from './components/Canvas.vue';
+// import { ref } from "vue";
+import axios from "axios";
+import Canvas from "./components/Canvas.vue";
+
+const uploadFile = async (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (input.files?.length) {
+    const selectedFile = input.files[0];
+
+    // FormData オブジェクトを作成してファイルを追加
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    // ファイルをサーバーにアップロード
+    axios
+      .post("http://localhost:5000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        // レスポンスを処理
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // エラー処理
+        console.error("ファイルのアップロードに失敗しました:", error);
+      });
+  }
+};
 </script>
 
 <template>
+  <input type="file" @change="uploadFile" class="fileInput" />
   <Canvas></Canvas>
 </template>
 
 <style scoped>
+.fileInput {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  z-index: 1;
+}
 </style>
